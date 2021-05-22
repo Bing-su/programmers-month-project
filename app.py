022ecb41a58a7bf7fa6c4838801516b2e441f9aa
Bing-su <37621276+Bing-su@users.noreply.github.com>
2021-05-22 19:38:39 +0900

    update

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-st.markdonw("#Hello, World!")
+st.markdown("# Hello, World!")
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"ks2515@naver.com\"\n\
+email = \"test@test.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
